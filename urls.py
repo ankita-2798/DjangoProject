94820d7ee93e3456e474d496b760b5e1aab616df
ankita-2798 <ankitabansal2798@gmail.com>
2016-07-01 16:53:52 +0530

    added urls for signup, login , changepassword , editprofile, logout

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns,url
+from polls import views
+from django.contrib.auth.decorators import login_required
+urlpatterns=patterns('',
+					url(r'^$',views.IndexView.as_view(),name='index'),
+					url(r'^(?P<pk>\d+)/$',views.DetailView.as_view(),name='detail'),
+					url(r'^(?P<pk>\d+)/results/$',views.ResultsView.as_view(),name='results'),
+					url(r'^(?P<question_id>\d+)/vote/$',views.vote,name='vote'),
+					url(r'^signup/$', views.call_signup, name='call_signup'),
+					url(r'^login/$', views.call_login, name='call_login'),
+					url(r'^createuser/$', views.create_user, name='create_user'),
+					url(r'^verifyuser/$', views.verify_user, name='verify_user'),
+					url(r'^changepassword/$', views.call_changepassword, name='call_changepassword'),
+					url(r'^savepassword/$', views.save_password, name='save_password'),
+)
